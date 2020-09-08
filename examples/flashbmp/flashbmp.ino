2588965aca924a746906261291f4358c691bc840
Mike Kushnerik <59262898+mkengineering@users.noreply.github.com>
2020-09-07 19:21:33 -0500

    Add files via upload

---
+++ examples/flashbmp/flashbmp.ino
@@ -0,0 +1,35 @@
+// ILI9341 example with embedded color bitmaps in sketch.
+// WILL NOT FIT ON ARDUINO UNO OR OTHER AVR BOARDS;
+// uses large bitmap image stored in array!
+
+// Options for converting images to the format used here include:
+//   http://www.rinkydinkelectronics.com/t_imageconverter565.php
+// or
+//  GIMP (https://www.gimp.org/) as follows:
+//    1. File -> Export As
+//    2. In Export Image dialog, use 'C source code (*.c)' as filetype.
+//    3. Press export to get the export options dialog.
+//    4. Type the desired variable name into the 'prefixed name' box.
+//    5. Uncheck 'GLIB types (guint8*)'
+//    6. Check 'Save as RGB565 (16-bit)'
+//    7. Press export to save your image.
+//  Assuming 'image_name' was typed in the 'prefixed name' box of step 4,
+//  you can have to include the c file, then using the image can be done with:
+//    tft.drawRGBBitmap(0, 0, image_name.pixel_data, image_name.width, image_name.height);
+//  See also https://forum.pjrc.com/threads/35575-Export-for-ILI9341_t3-with-GIMP
+
+//#include "SPI.h"
+#include <STM32_ILI9486_8_bit.h>
+#include "mke.c"
+
+STM32_ILI9486_8_bit tft;
+
+void setup() {
+  tft.begin();
+  tft.setRotation(0);
+  tft.fillScreen(WHITE);
+  tft.drawRGBBitmap(85, 181, (uint16_t *)gimp_image.pixel_data, 150, 118);
+}
+
+void loop(void) {
+  }
